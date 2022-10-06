f63aacd201cda987ccfa545b1ed1aa2707478c17
zarlorcode <lorena.zarapo@gmail.com>
2022-10-06 08:10:57 +0200

    Creation of class User

--- LabSolution/ClassLibrary/BusinessLogic/Entities/User.cs
+++ LabSolution/ClassLibrary/BusinessLogic/Entities/User.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Magazine.Entities
 {
-    public partial class User
+    public partial class User : Person
     {
+        public User(){ //Falta la llamada al constructor padre?? :base(.....) que no se si aquí hace falta
+            
+            MainAuthoredPapers = new List<Paper>();
+        }
+
+        public User(String id, String name,String surname, bool alerted, String areasOfInterest, 
+            String email, String login, String password, 
+            Area area, Magazine magazine)/*:base(id,name,surname)*/ {
+
+            this.Alerted = alerted;
+            this.AreasOfInterest = areasOfInterest;
+            this.Email = email;
+            this.Login = login;
+            this.Password = password;
+            this.Area = area;
+            this.Magazine = magazine;
+        }
     }
 }
--- LabSolution/ClassLibrary/Persistence/Entities/User.cs
+++ LabSolution/ClassLibrary/Persistence/Entities/User.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Magazine.Entities
 {
-    public partial class User
+    public partial class User : Person
     {
+        public bool Alerted { get; set; }
+        public String AreasOfInterest { get; set; }
+        public String Email { get; set; }
+
+        public String Login { get; set; }
+
+        public String Password { get; set; }
+
+        public virtual ICollection<Paper> MainAuthoredPapers
+        {
+            get;
+            set;
+        }
+
+        public virtual Magazine Magazine
+        {
+            get;
+            set;
+        }
+
+        public virtual Area Area
+        {
+            get;
+            set;
+        }
+
     }
 }
