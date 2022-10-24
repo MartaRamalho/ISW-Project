f22b8c7ecb517f236aacd3941bcde1c684155c0a
Ángel Burgos Muñoz <ABURMUO@upv.edu.es>
2022-10-24 18:51:39 +0200

    DBTest creada. ¡Falta Program.cs!

---
+++ LabSolution/DBTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Magazine.Entities;
+using Magazine.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTest
+{
+    internal class Program
+    {
+        static void Main()
+        {
+         
+        }
+        
+        private void createSampleDB(IDAL dal)
+        {
+            // Remove all data from DB
+            dal.RemoveAllData();
+
+            Console.WriteLine("Creando los datos y almacenándolos en la DB");
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine("\n// CREACIÓN DE UNA REVISTA Y SU EDITOR EN JEFE");
+            User u1 = new User("1234", "Pepe", "TheBoss", false, "ninguna", "pgarcia@gmail.com", "theboss", "1234");
+            dal.Insert<User>(u1);
+            dal.Commit();
+
+            Magazine.Entities.Magazine m = new Magazine.Entities.Magazine("Revista Universitaria", u1);
+            u1.Magazine = m;
+
+            dal.Insert<Magazine.Entities.Magazine>(m);
+            dal.Commit();
+
+            Console.WriteLine("Nombre de la revista: " + m.Name);
+            Console.WriteLine(" Editor de la revista: " + m.ChiefEditor.Name + " " + m.ChiefEditor.Surname);
+
+            Console.ReadKey();
+
+            // Populate here the rest of the database with data
+        }
+    }
+}
