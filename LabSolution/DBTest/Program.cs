682ae20bdcce8c9eada7e07c82e08213918f0842
Ángel Burgos Muñoz <angburmun@gmail.com>
2022-10-27 17:19:22 +0200

    Inclusión del Program.cs

--- LabSolution/DBTest/Program.cs
+++ LabSolution/DBTest/Program.cs
@@ -1,26 +1,74 @@
-﻿using Magazine.Entities;
-using Magazine.Persistence;
-using System;
+﻿using System;
+using System.Data.Entity.Validation;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Magazine.Entities;
+using Magazine.Persistence;
+
+
 namespace DBTest
 {
-    internal class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-         
-        }
-        
-        private void createSampleDB(IDAL dal)
-        {
-            // Remove all data from DB
-            dal.RemoveAllData();
 
-            Console.WriteLine("Creando los datos y almacenándolos en la DB");
+            try
+            {
+                new Program();
+            }
+            catch (Exception e)
+            {
+                printError(e);
+            }
+            Console.WriteLine("\nPulse una tecla para salir");
+            Console.ReadLine();
+        }
+
+        static void printError(Exception e)
+        {
+            while (e != null)
+            {
+                if (e is DbEntityValidationException)
+                {
+                    DbEntityValidationException dbe = (DbEntityValidationException)e;
+
+                    foreach (var eve in dbe.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                                ve.PropertyName,
+                                eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                                ve.ErrorMessage);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+                e = e.InnerException;
+            }
+        }
+
+
+        Program()
+        {
+            IDAL dal = new EntityFrameworkDAL(new MagazineDbContext());
+
+            CreateSampleDB(dal);
+        }
+
+        private void CreateSampleDB(IDAL dal)
+        {
+            dal.RemoveAllData();
+            Console.WriteLine("Creando los datos y almacenándolos en la BD");
             Console.WriteLine("===========================================");
 
             Console.WriteLine("\n// CREACIÓN DE UNA REVISTA Y SU EDITOR EN JEFE");
@@ -35,11 +83,14 @@ namespace DBTest
             dal.Commit();
 
             Console.WriteLine("Nombre de la revista: " + m.Name);
-            Console.WriteLine(" Editor de la revista: " + m.ChiefEditor.Name + " " + m.ChiefEditor.Surname);
+            Console.WriteLine("  Editor de la revista: " + m.ChiefEditor.Name + " " + m.ChiefEditor.Surname);
 
             Console.ReadKey();
 
             // Populate here the rest of the database with data
+
         }
+
     }
+
 }
