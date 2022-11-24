865b01d066a9ee59db9531251bd2875c23952774
zarlorcode <lorena.zarapo@gmail.com>
2022-11-24 17:50:53 +0100

    Finished PublishedPaper

--- LabSolution/MagazineLib/BusinessLogic/Services/MagazineISWService.cs
+++ LabSolution/MagazineLib/BusinessLogic/Services/MagazineISWService.cs
@@ -175,18 +175,18 @@ namespace Magazine.Services
         {
             Paper paper = magazine.GetPaperById(paperId);
             if (isPublicationPending(paperId))
-            {   
-                
+            {
                 Area pubPend = paper.PublicationPendingArea;
                 pubPend.PublicationPending.Remove(paper);
-            }
+                paper.PublicationPendingArea = null;           
+             }
             throw new ServiceException(resourceManager.GetString("PaperAlreadyPublished"));
         }
 
         public void UnPublishPaper(int paperId) {
             if (!isPublicationPending(paperId))
             {
-                Paper paper = magazine.getPaperById(paperId);
+                Paper paper = magazine.GetPaperById(paperId);
                 Area pubPend = paper.PublicationPendingArea;
                 pubPend.PublicationPending.Add(paper);
             }
